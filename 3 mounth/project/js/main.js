adcce77e88afe3ef4dde46bf213bac748136d7f1
Samish-chg <132761633+Samish-chg@users.noreply.github.com>
2023-07-12 16:25:11 +0600

    dz

---
+++ 3 mounth/project/js/main.js
@@ -0,0 +1,87 @@
+//RANDOM COLOR GENERATOR
+
+const buttonsColor = document.querySelectorAll('.btn-color')
+const javaScript = document.querySelector('#js-color')
+
+const generateRandomColor = () => {
+    const hexCodes = '0123456789ABCDEF'
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+const setRandomColors = () => {
+    buttonsColor.forEach((buttonColor) => {
+        buttonColor.innerHTML = generateRandomColor()
+        buttonColor.onclick = (event) => javaScript.style.color = event.target.innerHTML
+    })
+}
+
+window.onload = () => setRandomColors()
+window.onkeydown = (event) => {
+    if (event.code.toLowerCase() === 'space') {
+        event.preventDefault()
+        setRandomColors()
+    }
+}
+
+// SLIDER BLOCK
+
+const slides = document.querySelectorAll('.slide')
+
+const hideSlide = () => {
+    slides.forEach((slide) => {
+        slide.style.opacity = 0
+        slide.classList.remove('active_slide')
+    })
+}
+const showSlide = (i = 0) => {
+    slides[i].style.opacity = 1
+    slides[i].classList.add('active_slide')
+}
+
+hideSlide()
+showSlide()
+
+const autoSlider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > slides.length - 1) {
+            i = 0
+        }
+        hideSlide()
+        showSlide(i)
+    }, 6000)
+}
+autoSlider()
+
+let position =0
+function moveMusk(){
+    let box = document.querySelector('.child_block')
+    box.style.left=`${position}px`
+    position++
+    if(position<1100){
+        requestAnimationFrame(moveMusk)
+    }
+
+}
+moveMusk()
+
+// REG EXP
+const gmailInput = document.querySelector('#gmailInput')
+const gmailCheck = document.querySelector('#gmailCheck')
+const gmailResult = document.querySelector('.gmailResult')
+
+const regExp = /^[a-zA-Z0-9._-]+@gmail\.com$/
+
+gmailCheck.onclick = () => {
+    if (regExp.test(gmailInput.value)) {
+        gmailResult.innerHTML = 'OK'
+        gmailResult.style.color = 'green'
+    } else {
+        gmailResult.innerHTML = 'NOT OK'
+        gmailResult.style.color = 'red'
+    }
+}
